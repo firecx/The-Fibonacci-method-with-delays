86e3238327f8482bf1cc50340221da4243c2a4b5
nikan <nikan@LAPTOP-A3G49DOD>
2024-04-18 08:22:00 +0600

    Добавьте файлы проекта.

--- Form1.cs
+++ Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace The_Fibonacci_method_with_delays
 {
@@ -17,9 +19,10 @@ namespace The_Fibonacci_method_with_delays
             InitializeComponent();
         }
 
-        //Список для генерации псевдослучайных чисел по методу Фибаначи с запозданием
+        //Список (задержка) для генерации псевдослучайных чисел по методу Фибаначи с запозданием
         List<int> numbersDictionary = new List<int>(56);
 
+        //Список содержащий генерируемые числа
         List<int> numbers = new List<int>();
 
         //Стандартный генератор псевдослучайных чисел
@@ -34,19 +37,21 @@ namespace The_Fibonacci_method_with_delays
             }
         }
 
+
         //Реализация генерации псевдослучайных чисел по методу Фибаначи с запозданием
         private int random_Fibonacci(int min, int max, List<int> numDictionary)
         {
             int num;
 
             //Формула генерации псведослучайного числа
-            num  = (Math.Abs(numDictionary.ElementAt(24) + numDictionary.ElementAt(52))) % (max - min + 1) + min;
+            num = (Math.Abs(numDictionary.ElementAt(24) + numDictionary.ElementAt(52))) % (max - min + 1) + min;
             
             numDictionary.RemoveAt(0);
             numDictionary.Add(num);
             
             return num;
         }
+
 
         private void start_Click(object sender, EventArgs e)
         {
@@ -59,26 +64,34 @@ namespace The_Fibonacci_method_with_delays
                 numbers.Add(random_Fibonacci((int)minValue.Value, (int)maxValue.Value, numbersDictionary));
             }
 
+            List_to_text();
+            Analysis();
+        }
+
+        //Вывод чисел в TextBox
+        private void List_to_text()
+        {
+            string numbersSrting = string.Empty;
             foreach (var item in numbers)
             {
-                numbersSrting += item + ", ";
+                numbersSrting += item + "; ";
             }
 
             field.Text = numbersSrting;
-            
-
         }
-        //Анализ
-        private void analysis_Click(object sender, EventArgs e)
+
+        //Анализ распределения сгенерированных чисел
+        private void Analysis()
         {
-            int[] numAnalysis = new int[(int)maxValue.Value + 1];
+            int[] numAnalysis = new int[(int)maxValue.Value + 1]; // i - число, numAnalysis[i] - количество чисел i
+            List<int> list = numbers;
+            list.Sort();
             for (int i = 0; i <= (int)maxValue.Value; i++)
             {
-                numbers.Sort();
-                while (numbers.Contains(i))
+                while (list.Contains(i))
                 {
                     numAnalysis[i]++;
-                    numbers.Remove(i);
+                    list.Remove(i);
                 }
             }
 
@@ -87,7 +100,35 @@ namespace The_Fibonacci_method_with_delays
             {
                 chart.Series[0].Points.AddXY(i, numAnalysis[i]);
             }
+        }
 
+
+        //Переключение между гистограммой и списком чисел
+        private void Switch_Click(object sender, EventArgs e)
+        {
+            if (chart.Visible == false)
+            {
+                chart.Visible = true;
+                SwitchButton.Text = "Список";
+            }
+            else
+            {
+                chart.Visible = false;
+                SwitchButton.Text = "Анализ";
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
+                streamWriter.WriteLine(field.Text);
+                streamWriter.Close();
+            }
         }
     }
 }
